50ad7908a32416b422ba51c4c54048b4b60cd5ac
Asraful Haque <asrafulhaque2114@gmail.com>
2026-08-09 19:41:19 +0530

    Delete Dictionary.py

--- Dictionary.py
+++
@@ -1,7 +0,0 @@
-# Create a dictionary employee that the value against empoyee id is a nested dictionary that include emp name,
-# designation, dept and salary. The dict contains records of 5 employees.
-#Perform the following oprn on the dict:
-# 1. Print the record of emp with emp id E1.
-# 2. Print the dept of emp E4.
-# 3. Print the record of emp having max salary.
-# 4. Insert a new emp record in the existing dict.
